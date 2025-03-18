2f71f52a16c02ed17a72d1803cf1a2e16b88bc74
Ajunie Zeng <ajuniezeng@gmail.com>
2025-03-17 22:38:05 -0700

    adapt to aarch32 toolchain

--- toolchain.cmake
+++ toolchain.cmake
@@ -2,8 +2,8 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR armv7l)
 set(CMAKE_CROSSCOMPILING TRUE)
 
-set(CMAKE_C_COMPILER clang)
-set(CMAKE_CXX_COMPILER clang++)
+set(CMAKE_C_COMPILER arm-linux-gnueabi-gcc)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabi-g++)
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --target=arm-linux-gnueabi")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --target=arm-linux-gnueabi")
