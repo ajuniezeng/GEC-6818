51887da5a4bfba57f8ac76a09cc129708f20b5fd
Ajunie Zeng <ajuniezeng@gmail.com>
2025-03-19 03:13:55 -0700

    homework 3

--- app/main.c
+++ app/main.c
@@ -5,16 +5,37 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
-#include "bmp.h"
+// #include "bmp.h"
 #include "lcd_control.h"
+#include "render.h"
 
 int main(void) {
   struct LCD lcd;
   lcd_new(&lcd);
   lcd.clear(&lcd);
-  lcd.draw_dark_full_screen(&lcd);
+  for (size_t i = 0; i < SCREEN_HEIGHT; i++) {
+    for (size_t j = 0; j < SCREEN_WIDTH; j++) {
+      lcd.draw_pixel(&lcd, i, j, TEAL);
+    }
+  }
 
-  render_bmp(&lcd, "pic/pic2.bmp", 100, 200);
+  for (size_t i = 6; i < 20; i++) {
+    render_zh_cn_character(&lcd, i, 50, 20 + (i - 6) * 32 , BLACK, TEAL);
+  }
+
+  render_string(&lcd, "By", 380, 620, BLACK, TEAL);
+
+  for (size_t i = 20; i < 23; i++) {
+    render_zh_cn_character(&lcd, i, 380, 640 + (i - 20) * 32 , BLACK, TEAL);
+  }
+
+  render_string(&lcd, "20220440333", 380, 700, BLACK, TEAL);
+  
+  for (size_t i = 23; i < 26; i++) {
+    render_zh_cn_character(&lcd, i, 400, 640 + (i - 23) * 32 , BLACK, TEAL);
+  }
+
+  render_string(&lcd, "20220440333", 400, 700, BLACK, TEAL);
 
   lcd_destructor(&lcd);
   return 0;
